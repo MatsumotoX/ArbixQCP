8b4d4e71f1b4bc6a33a81306f1e2dd9bc05a226e
WorakornX <p.worakorn@gmail.com>
2018-02-25 22:10:52 +0700

    add webhook

--- app/Http/Controllers/LineWebbhook.php
+++ app/Http/Controllers/LineWebbhook.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\LineWebhook;
 
 class LineWebbhook extends Controller
 {
@@ -27,52 +28,18 @@ class LineWebbhook extends Controller
         $access_token = 'pToloaOV2TgYIOjrnWPWnsoBuVWrbIHsyGd0FygG902nwCXDrxE46Hx3c+tvNx1eiUpBDi1JNNH+sidjRAoTFbYPyCgmUCAQOTAYSdiEwCLHnMrMLEYOpNtv7F4lagtp8WJORXx0A8iYuEdmhTPEcwdB04t89/1O/w1cDnyilFU=';
 
         // Get POST body content
-        $content = $request;
+        $content = file_get_contents('php://input');
         // Parse JSON
         $events = json_decode($content, true);
         // Validate parsed JSON data
-        if (!is_null($events['events'])) {
-            // Loop through each event
-            foreach ($events['events'] as $event) {
-                // Reply only when message sent is in 'text' format
-                if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-                    // Get text sent
-                    $text = $event['message']['text'];
-                    // Get replyToken
-                    $replyToken = $event['replyToken'];
+        
+        $replyToken = $event['replyToken'];
 
-                    // Build message to reply back
-                    $messages = [
-                        
-                        'type' => 'text',
-                        'text' => $event['source']['groupId']
-                        
-                    ];
-
-                    // Make a POST Request to Messaging API to reply to sender
-                    $url = 'https://api.line.me/v2/bot/message/reply';
-                    $data = [
-                        'replyToken' => $replyToken,
-                        'messages' => [$messages],
-                    ];
-                    $post = json_encode($data);
-                    $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-                    $ch = curl_init($url);
-                    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-                    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-                    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-                    $result = curl_exec($ch);
-                    curl_close($ch);
-
-                    echo $result . "\r\n";
-            
-
-		}
+        $line = new MarketFee;
+        $line->replyToken = $replyToken;
+        $line->save();
+	
 	}
 }
-echo "OK";
-    }
-}
+
+
