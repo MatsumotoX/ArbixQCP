8743f0915abef7dc7cea0fc51eb0baaaeb6ccb90
MatsumotoX <mat@cryptovation.co>
2018-03-04 06:21:55 +0700

    font

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,43 +34,55 @@
 			<div class="row">
 				
 				<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 bottomReveal">
-					<ul class="planPrice plan1 plan">
+					<ul class="plan plan1 plan">
 						<img src="{{ asset('/image/Price.png') }}" class="logo-size"></img>
 						<li class="plan-name">Live Price</li>
+						<li>BTC : $11,500 </li>
+						<li>ETH : $ 800 </li>
+						<li>XRP : $ 0.9000 </li>
 						<li class="plan-price">
-							<a class="btn btn-defaultPrice btn-lg" href="#">Use Now!</a>
+							<a class="btn btn-default btn-lg" href="#">Use Now!</a>
 						</li>
 					</ul>
 				</div>
 
 
 				<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 mt30-xs topReveal">
-					<ul class="planOne plan2 plan">
+					<ul class="plan plan2 plan">
 						<img src="{{ asset('/image/Oneway4.png') }}" class="logo-size"></img>
 						<li class="plan-name">One-way</li>
+						<li> Profit 10% / Time </li>
+						<li> 2 Times / Months </li>
+						<li> Limit - </li>
 						<li class="plan-price">
-							<a class="btn btn-defaultOne btn-lg" href="#">Use Now!</a>
+							<a class="btn btn-default btn-lg" href="#">Use Now!</a>
 						</li>
 					</ul>
 				</div>
 
 				<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 mt30-sm mt30-xs bottomReveal">
-					<ul class="planLoop plan3 plan">
+					<ul class="plan plan3 plan">
 						<img src="{{ asset('/image/Loop.png') }}" class="logo-size"></img>
 						<li class="plan-name">Loop</li>
+						<li> Profit 5% / Time </li>
+						<li> 4 Times / Month </li>
+						<li> Limit - </li>
 						<li class="plan-price">
-							<a class="btn btn-defaultLoop btn-lg" href="#">Use Now!</a>
+							<a class="btn btn-default btn-lg" href="#">Use Now!</a>
 						</li>
 					</ul>
 				</div>
 
 
 				<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 mt30-sm mt30-xs topReveal">
-					<ul class="planTri plan4 plan">
+					<ul class="plan plan4 plan">
 						<img src="{{ asset('/image/Tri3.png') }}" class="logo-size"></img>
 						<li class="plan-name">Tri-angle</li>
+						<li> Profit 5% / Time </li>
+						<li> 1 Time / Month </li>
+						<li> Limit - </li>
 						<li class="plan-price">
-							<a class="btn btn-defaultTri btn-lg" href="#">Use Now!</a>
+							<a class="btn btn-default btn-lg" href="#">Use Now!</a>
 						</li>
 					</ul>
 				</div>
@@ -78,11 +90,14 @@
 				
 				<!-- Item 1 -->
 				<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 bottomReveal">
-						<ul class="planRed plan1 plan5 plan">
+						<ul class="plan plan1 plan5 plan">
 							<img src="{{ asset('/image/Red2.png') }}" class="logo-size"></img>
 							<li class="plan-name">Red Phoenix</li>
+							<li> Profit 0.3% / Time </li>
+							<li> 50 Times / Months </li>
+							<li> Limit - </li>
 							<li class="plan-price">
-								<a class="btn btn-defaultRed btn-lg" href="#">Use Now!</a>
+								<a class="btn btn-default btn-lg" href="#">Use Now!</a>
 							</li>
 						</ul>
 					</div>
@@ -90,33 +105,42 @@
 	
 					<!-- Item 2 -->
 					<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 mt30-xs topReveal">
-						<ul class="planBlack plan2 plan6 plan ">
+						<ul class="plan plan2 plan6 plan ">
 							<img src="{{ asset('/image/Black1.png') }}" class="logo-size"></img>
 							<li class="plan-name">Black Panter</li>
+							<li> Profit 0.3% / Time </li>
+							<li> 40 Times / Months </li>
+							<li> Limit - </li>
 							<li class="plan-price">
-								<a class="btn btn-defaultBlack btn-lg" href="#">Use Now!</a>
+								<a class="btn btn-default btn-lg" href="#">Use Now!</a>
 							</li>
 						</ul>
 					</div>
 	
 					<!-- Item 3 -->
 					<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 mt30-sm mt30-xs bottomReveal">
-						<ul class="planBlue plan3 plan7 plan">
+						<ul class="plan plan3 plan7 plan">
 							<img src="{{ asset('/image/Blue3.png') }}" class="logo-size"></img>
 							<li class="plan-name">Whale Catcher</li>
+							<li> Profit 0.5% / Time </li>
+							<li> 5 Times / Months </li>
+							<li> Limit - </li>
 							<li class="plan-price">
-								<a class="btn btn-defaultBlue btn-lg" href="#">Use Now!</a>
+								<a class="btn btn-default btn-lg" href="#">Use Now!</a>
 							</li>
 						</ul>
 					</div>
 	
 					<!-- Item 4 -->
 					<div class="col-xs-12 col-sm-6 col-md-3 col-lg-3 mt30-sm mt30-xs topReveal">
-						<ul class="planGreen plan4 plan">
+						<ul class="plan plan4 plan">
 							<img src="{{ asset('/image/Green1.png') }}" class="logo-size"></img>
 							<li class="plan-name">Green Python</li>
+							<li> Profit 3% / Time </li>
+							<li> 1 Times / Months </li>
+							<li> Limit - </li>
 							<li class="plan-price plan8">
-								<a class="btn btn-defaultGreen btn-lg" href="#">Use Now!</a>
+								<a class="btn btn-default btn-lg" href="#">Use Now!</a>
 							</li>
 						</ul>
 					</div>
